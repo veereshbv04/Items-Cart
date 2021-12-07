c4d87232f20cb76108a17e43e4dde657a0209309
Veeresh B V <Veereshbv04@gmail.com>
2021-12-07 19:39:20 +0530

    search functionality is added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const userItem = document.getElementById("userInput");
 const addBtn = document.getElementById("addBtn");
-const parentDiv = document.querySelector(".item-list");
+const ulDiv = document.querySelector(".item-list");
+const serachItem = document.getElementById("searchItem");
+const liDiv = document.querySelectorAll(".item");
+
 
 addBtn.addEventListener("click", (event)=>{
     
@@ -11,7 +14,20 @@ addBtn.addEventListener("click", (event)=>{
     const text = document.createTextNode(userInput);
     div.appendChild(text);
     div.classList.add("item");
-    parentDiv.appendChild(div);
+    ulDiv.appendChild(div);
      
 })
     
+serachItem.addEventListener("keyup", () =>{
+    
+    var searchItem = serachItem.value.toUpperCase();
+    for(let i=0; i<liDiv.length; i++){
+        var liItem = liDiv[i].innerText.toUpperCase();
+        if(liItem.includes(searchItem)){
+            liDiv[i].style.display = "block";
+        }else{
+            liDiv[i].style.display = "none";
+        }
+    }
+
+})
